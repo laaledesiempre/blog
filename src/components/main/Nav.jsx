579959ac398d5639d99060e27fa8d2e13169e1af
Alejandra Botacura <alejandrabotacura@outlook.com.ar>
2023-06-15 19:03:04 -0300

    commit 4 update on local

--- src/components/main/Nav.jsx
+++ src/components/main/Nav.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export const Nav = () => {
+
   return (
-    <div>Nav</div>
+  <div className='nav-container'>
+      <button className='nav-button'>.nav</button>
+    <div className='options-wrapper'>
+      <button className='option-element'>Home</button>
+      <button className='option-element'>Articles</button>
+      <button className='option-element'>Resume</button>
+    </div>
+  </div>
   )
 }
