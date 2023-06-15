33c6ddb7b432b2427ee6c69a012641dd306f0a3c
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-06-15 14:34:56 -0300

    added nav pre

--- src/components/main/Nav.jsx
+++ src/components/main/Nav.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
+/*
+  <div class=navigator>
+      <button class="nav" onclick="change()">.nav</button>
+  <div class="Opt">
+     <button class="hidden nav-el nav1">/home</button>
+     <button class="hidden nav-el nav2">/blog</button>
+     <button class="hidden nav-el nav3">/cv</button>
 
+  </div>
+  </div>
+
+const change= ()=>{
+  document.querySelectorAll(".nav-el").forEach(e=> {e.classList.toggle("hidden")})
+}
+*/ 
 export const Nav = () => {
   return (
     <div>Nav</div>
