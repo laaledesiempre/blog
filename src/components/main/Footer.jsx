d30c84538a2230ae1fb7952e9578876fcecfeda1
Alejandra Botacura <alejandrabotacura@outlook.com.ar>
2023-06-16 19:42:26 -0300

    Added footer swap to components and styles

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -5,7 +5,21 @@ export const Footer = () => {
     <>
       <footer>
         <div className="footer-background">
-
+          <div class="hidder"></div>
+          <div class="background">
+              <div class="num-el an">
+                <img class="" src="https://github.com/laaledesiempre/blog/blob/main/src/images/1.png?raw=true"/>
+                <img class="" src="https://github.com/laaledesiempre/blog/blob/main/src/images/1.png?raw=true"/>
+              </div>
+              <div class="num-el en">
+                <img class="" src="https://github.com/laaledesiempre/blog/blob/main/src/images/2.png?raw=true"/>
+                <img class="" src="https://github.com/laaledesiempre/blog/blob/main/src/images/2.png?raw=true"/>
+                </div>
+              <div class="num-el in">
+                <img class="" src="https://github.com/laaledesiempre/blog/blob/main/src/images/3.png?raw=true"/>
+                <img class="" src="https://github.com/laaledesiempre/blog/blob/main/src/images/3.png?raw=true"/>
+              </div>
+          </div> 
         </div>
         <div className="footer-wrapper">
           <h4 className="footer-thanks">Thanks for reading!</h4>
